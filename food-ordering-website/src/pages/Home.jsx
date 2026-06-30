a0762debcc3a1c530536219ab5ab68537501eb7a
Dharshini-Prog <ssdharshini.it@gmail.com>
2026-06-30 11:48:55 +0530

    Hero section done

---
+++ food-ordering-website/src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import heroImg from "../assets/hero.png";
+
+function Home() {
+  return (
+    <section className="hero">
+
+      <div className="hero-text">
+        <h1>Delicious Food Delivered To Your Door</h1>
+
+        <p>
+          Fresh meals, fast delivery, and amazing offers every day.
+          Explore our menu and satisfy your cravings.
+        </p>
+
+        <button>Order Now</button>
+      </div>
+
+      <div className="hero-image">
+        <img src={heroImg} alt="Delicious food" />
+      </div>
+
+    </section>
+  );
+}
+
+export default Home;
